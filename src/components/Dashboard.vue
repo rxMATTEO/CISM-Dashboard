2a7c596482b87f2f9f30f937797b1eb2a53d78f9
rxMATTEO <erik_sundberg@mail.ru>
2023-09-22 14:24:52 +0300

    Change styles on dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import {ref} from 'vue';
 
+const searchResult = ref('Ничего не найдено');
 </script>
 
 <template>
@@ -10,7 +12,16 @@
       <input type="text" class="search" placeholder="Введите ID документа">
     </div>
     <div class="dashboard-main">
+      <div class="dashboard-header">
+        <p class="header">Результаты</p>
+        <p class="search-result">{{ searchResult }}</p>
+      </div>
+    </div>
+  </div>
 
+  <div class="dashboard-content">
+    <div class="dashboard-content-empty">
+      <p>Выберите документ, чтобы посмотреть его содержимое</p>
     </div>
   </div>
 </div>
@@ -21,7 +32,14 @@
   box-shadow: 4px 0 8px 4px rgba(0, 0, 0, 0.2);
   margin-top: 1rem;
   border-radius: .5rem;
-  padding: 1rem;
+  height: calc(100vh - 100px);
+  & > div {
+    padding: 1rem;
+  }
+  .dashboard-side {
+    border-right: var(--secondary) solid 1px;
+  }
+  display: flex;
   .dashboard-header {
     .header {
       font-weight: bold;
@@ -29,10 +47,33 @@
     .search {
       background-color: white;
       border: 2px var(--secondary) solid;
-      padding: .8rem;
+      padding: 16px 24px 16px 24px;
       border-radius: 5px;
       color: black;
       margin-top: 5px;
+      &::placeholder {
+        font-size: 14px;
+      }
+    }
+  }
+  .dashboard-main {
+    margin-top: 15px;
+    .search-result {
+      margin-top: 5px;
+      color: var(--text-secondary);
+      font-size: 14px;
+    }
+  }
+
+  .dashboard-content {
+    width: 100%;
+    .dashboard-content-empty {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 100%;
+      color: var(--text-secondary);
     }
   }
 }
