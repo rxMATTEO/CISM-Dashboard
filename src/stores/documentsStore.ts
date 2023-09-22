eb78a666058d6061f99dda1df5a46b8dd3ca12ba
rxMATTEO <erik_sundberg@mail.ru>
2023-09-22 14:48:01 +0300

    Add documents store

---
+++ src/stores/documentsStore.ts
@@ -0,0 +1,13 @@
+import {defineStore} from 'pinia';
+
+export const documentsStore = defineStore('documentsStore', {
+  state: () => {
+    return {};
+  },
+  actions : {
+
+  },
+  getters: {
+
+  }
+});
