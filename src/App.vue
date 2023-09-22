cb1f3a9d3b4001a589210b5ecb4b4cc83108d9c9
rxMATTEO <erik_sundberg@mail.ru>
2023-09-22 14:07:05 +0300

    Add some styles into dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
+import Dashboard from './components/Dashboard.vue';
 </script>
 
 <template>
   <div class="main">
     <Header />
+    <Dashboard />
   </div>
 </template>
 
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="dashboard">
+  <div class="dashboard-header">
+    <p class="header">Поиск документа</p>
+    <input type="text" class="search" placeholder="Введите ID документа">
+  </div>
+</div>
+</template>
+
+<style scoped>
+.dashboard {
+  box-shadow: 4px 0 8px 4px rgba(0, 0, 0, 0.2);
+  margin-top: 15px;
+  border-radius: 5px;
+  padding: 10px;
+  .dashboard-header {
+    .header {
+      font-weight: bold;
+    }
+    .search {
+      background-color: white;
+      border: 2px var(--secondary) solid;
+      padding: 10px;
+      border-radius: 5px;
+      color: black;
+      margin-top: 5px;
+    }
+  }
+}
+</style>
